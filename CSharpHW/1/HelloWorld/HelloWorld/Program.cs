5addc1fe49bfa07c14103b1aaabc17186b297e53
Kabawa <deymosmile@ukr.net>
2016-10-19 18:56:48 +0300

    Add first project

---
+++ CSharpHW/1/HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var name = "Sergey";
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Hello, " + name);
+
+            Console.ResetColor();
+            Console.ReadLine();
+        }
+    }
+}
